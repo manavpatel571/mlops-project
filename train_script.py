922669e4c9d3b0a0b19799d4c3a974e2a1bbbd3c
manavpatel571 <manavp571@gmail.com>
2025-11-05 15:42:25 +0530

    update3

--- train_script.py
+++ train_script.py
@@ -1,11 +1,18 @@
 import mlflow
 import mlflow.sklearn
+import dagshub
+from dotenv import load_dotenv
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
+
 print("🚀 Running training script...")
+
+# Initialize DagsHub MLflow tracking
+load_dotenv()
+dagshub.init(repo_owner="manavpatel571", repo_name="mlops-project", mlflow=True)
 
 # Load dataset
 iris = load_iris()
@@ -21,7 +28,6 @@ acc = accuracy_score(y_test, preds)
 print(f"✅ Model trained successfully. Accuracy: {acc:.2f}")
 
 # Log to MLflow (DagsHub)
-mlflow.set_tracking_uri("https://dagshub.com/manavpatel571/mlops-demo-student.mlflow")
 mlflow.set_experiment("CI-CD-Training")
 with mlflow.start_run(run_name="CI-CD-Pipeline-Run"):
     mlflow.log_param("model_type", "RandomForestClassifier")
